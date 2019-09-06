054ac4967699c31eb89f8505dc05e50759771d8a
Corina Allen <StarPerfect@gmail.com>
2019-09-06 09:59:32 -0600

    User Story 1

--- spec/features/user_can_see_an_individual_vending_machine_spec.rb
+++ spec/features/user_can_see_an_individual_vending_machine_spec.rb
@@ -10,3 +10,25 @@ RSpec.describe 'When a user visits a vending machine show page', type: :feature 
     expect(page).to have_content("Don's Mixed Drinks Vending Machine")
   end
 end
+# As a user
+# When I visit a vending machine show page
+# I see the name of all of the snacks associated with that vending machine along with their price
+require 'rails_helper'
+
+RSpec.describe 'Vending Machine Show Page' do
+  before :each do
+    @denver_vending = Owner.create(name: 'Denver Vending Machines, Inc.')
+    @turing = @denver_vending.machines.create(location: 'Turing')
+    @poptarts = @turing.snacks.create(name: 'Poptarts', price: 1.00)
+    @skittles = @turing.snacks.create(name: 'Skittles', price: 1.25)
+  end
+
+  it 'User sees all snacks and their price' do
+    visit machine_path(@turing)
+
+    expect(page). to have_content(@poptart.name)
+    expect(page). to have_content(@poptart.price)
+    expect(page). to have_content(@skittles.name)
+    expect(page). to have_content(@skittles.price)
+  end
+end
